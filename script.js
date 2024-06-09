2d875394bbdcabc678dbd238e9ff80174f2df28c
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-06-09 16:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const inc=document.getElementById('inc');
+const btn=document.getElementById('btn');
+
+let count=0;
+
+btn.addEventListener("click",function() {
+	alert('lockhost3000says')
+	count++;
+	inc.innerText=count;
+})
